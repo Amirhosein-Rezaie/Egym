732462852f9087884ff134e51e49d9d2ae7e0f04
amir <ah.rezaie07@gmail.com>
2025-06-03 22:08:12 +0330

    create the models (did not migrated)

--- products/models.py
+++ products/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.core.validators import (
+    MinValueValidator
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+
+    price = models.IntegerField(validators=[
+        MinValueValidator(0)
+    ])
+
+    description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_name = 'Product'
+
+    def __str__(self):
+        return self.title
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    class ROLES(models.TextChoices):
+        ADMIN = 'ADMIN'
+        USER = 'USER'
+
+    role = models.CharField(choices=ROLES.choices,
+                            default=ROLES.ADMIN, max_length=10)
+
+    class Meta:
+        db_name = 'User'
+
+    def __str__(self):
+        return self.username
