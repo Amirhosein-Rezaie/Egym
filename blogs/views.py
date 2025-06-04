f9e93bea3d1788c9966c48ee1c7f57a68831d3a7
amir <ah.rezaie07@gmail.com>
2025-06-04 12:33:42 +0330

    send the all of blogs and pagination of them

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from . import models
+from . import serializeres
 
-# Create your views here.
+
+class BlogPagination(PageNumberPagination):
+    page_size = 2
+
+
+class BlogAll(viewsets.ModelViewSet):
+    queryset = models.Blog.objects.all()
+    serializer_class = serializeres.BlogSerializer
+    pagination_class = BlogPagination
