a562821b4f0be7f5b406ca6b7cdbe24eb6e18ba1
amirhosein <amirhossein48x90@gmail.com>
2025-06-05 11:11:53 -0700

    send products with their images

--- products/serializers.py
+++ products/serializers.py
@@ -2,7 +2,15 @@ from rest_framework.serializers import ModelSerializer
 from . import models
 
 
+class ImageSerilizer(ModelSerializer):
+    class Meta:
+        model = models.Image
+        fields = ['id', 'file']
+
+
 class ProductSerializer(ModelSerializer):
+    images_product = ImageSerilizer(many=True, read_only=True)
+
     class Meta:
         model = models.Product
         fields = '__all__'
