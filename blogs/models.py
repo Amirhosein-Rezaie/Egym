5bffd008d4a03829452656e88ffcccecec558f99
amirhosein <amirhossein48x90@gmail.com>
2025-06-05 14:27:02 -0700

    add image field to blog model

--- blogs/models.py
+++ blogs/models.py
@@ -6,6 +6,7 @@ class Blog(models.Model):
         max_length=255, db_index=True, blank=False, null=False)
     content = models.TextField(blank=False, null=False)
     uploaded_at = models.DateField(auto_now_add=True, blank=False, null=False)
+    image = models.ImageField(null=True, blank=True, upload_to='images/')
 
     class Meta:
         db_table = 'blog'
