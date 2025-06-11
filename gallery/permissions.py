dc875a71eaa63b6f869e64f19e34ad0b8f20aeb3
amir <ah.rezaie07@gmail.com>
2025-06-11 17:39:40 +0330

    improving the permission of gallery

--- gallery/permissions.py
+++ gallery/permissions.py
@@ -8,6 +8,5 @@ class IsAdminOrReadonly(BasePermission):
     def has_permission(self, request, view):
         if (request.method in SAFE_METHODS):
             return True
-        if (request.method not in SAFE_METHODS):
+        else:
             return bool(request.user.is_staff == True)
-        return super().has_permission(request, view)
