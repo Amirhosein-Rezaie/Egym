06f9f7b9a93621a5a4a566d923a91d126c4c73bf
amirhosein <amirhossein48x90@gmail.com>
2025-06-11 15:57:27 -0700

    make migrations of gallery , migrate it and test

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    photo = models.ImageField(upload_to='gallery/')
+
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.photo} -> {self.uploaded_at}"
+    
+    class Meta:
+        db_table = 'gallery'
+        
